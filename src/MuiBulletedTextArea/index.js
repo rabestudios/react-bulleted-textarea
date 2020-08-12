c4158fb53a047587fb6fdf88558f5541dcf260f7
Chris Rabe <chris.rabe14@gmail.com>
2020-08-12 18:15:36 +1000

    updated on effect

--- src/MuiBulletedTextArea/index.js
+++ src/MuiBulletedTextArea/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import TextField from '@material-ui/core/TextField'
 import BulletAdornment from './BulletAdornment'
@@ -22,6 +22,10 @@ const MuiBulletedTextArea = (props) => {
     },
     [setValue, setLines, lines, onChange]
   )
+
+  useEffect(() => {
+    setValue(values.join('\n'))
+  }, [values])
 
   return (
     <TextField
